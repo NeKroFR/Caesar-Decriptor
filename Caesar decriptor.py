4816dc091ddce5f1ef478b23963a55f03e052e7e
NeKroFR <70316327+NeKroFR@users.noreply.github.com>
2020-11-19 14:04:53 +0100

    Update Caesar decriptor.py

--- Caesar decriptor.py
+++ Caesar decriptor.py
@@ -1,6 +1,13 @@
-text_base= input("Enter the encrypted  text here:")
+import string
 
-for i in range(25):
+alphabet = string.ascii_lowercase
+
+
+text_base= input("Enter the encrypted  text here:")
+decrypted_message = ""
+
+
+for i in range(26):
     print(text_base)
 
 input('press enter to leave')
