6b031c0ca99a2575b2b37f056c31a0ea1acc4cd8
NeKroFR <70316327+NeKroFR@users.noreply.github.com>
2021-02-20 13:25:00 +0100

    Update Caesar.py

--- Caesar.py
+++ Caesar.py
@@ -3,11 +3,20 @@ import string
 alphabet = string.ascii_lowercase
 
 
-text_base= input("Enter the encrypted  text here:")
-decrypted_message = ""
+text_base= input("Enter your  text here:")
+encrypted_message = ""
+letter =
 
+for i in text_base:
 
-for i in range(26):
-    print(text_base)
+        if i in alphabet:
+            position = alphabet.find(i)
+            new_position = letter+1
+            new_character = alphabet[new_position]
+            decrypted_message += new_character
+        else:
+            decrypted_message += i
 
-input('press enter to leave')
+print(encrypted_message)
+
+input('\npress enter to leave')
